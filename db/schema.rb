989f64e5f2481557a4a6b99b8817b137ab201f13
ClaytonSiby <claytonsiby@gmail.com>
2021-04-25 20:26:48 +0200

    Remove user column from the suits table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_135441) do
+ActiveRecord::Schema.define(version: 2021_04_25_141721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2021_04_24_135441) do
     t.string "size"
     t.string "description"
     t.string "imageUrl"
-    t.string "user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
