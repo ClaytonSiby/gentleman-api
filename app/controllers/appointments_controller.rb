61681d0a75b0e9cfcf093e28a94a4b8f235831d8
ClaytonSiby <claytonsiby@gmail.com>
2021-05-04 00:00:54 +0200

    Remove all commented out code from the appointment controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -19,45 +19,4 @@ class AppointmentsController < ApplicationController
       render json: { message: 'Something went wrong' }
     end
   end
-
-  # GET /suits/:suit_id/appointments
-  # def index
-  #   json_response(@suit.appointments)
-  # end
-
-  # def show
-  #   json_response(@appointment)
-  # end
-
-  # # POST /suits/:suit_id/appointments/:id
-  # def create
-  #   @suit.appointments.create!(appointment_params)
-  #   json_response(@suit, :created)
-  # end
-
-  # # PUT /suits/:suit_id/appointments/:id
-  # def update
-  #   @appointment.update(appointment_params)
-  #   head :no_content
-  # end
-
-  # # DELETE /suits/:suit_id/appointments/:id
-  # def destroy
-  #   @appointment.destroy
-  #   head :no_content
-  # end
-
-  # private
-
-  # def appointment_params
-  #   params.permit(:user_id, :suit_id)
-  # end
-
-  # def set_suit
-  #   @suit = Suit.find(params[:suit_id])
-  # end
-
-  # def set_suit_appointment
-  #   @appointment = @suit.appointments.find_by!(id: params[:id]) if @suit
-  # end
 end
