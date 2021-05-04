cbd3ba8cf8560fe227df0d82ee08509fd519422b
ClaytonSiby <claytonsiby@gmail.com>
2021-05-04 19:58:11 +0200

    Update the ApiVersion class and the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     # namespace the controllers without affecting the URI
     scope module: :v1, contraints: ApiVersion.new('v1', true) do
       resources :suits
-      resources :appointments
+      resources :appointments, only: %i[create destroy]
     end
 
     post '/auth/login', to: 'authentication#authenticate'
