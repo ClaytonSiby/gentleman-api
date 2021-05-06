70d2fa0395d42fd8d248978e8713f3df3355c3b9
ClaytonSiby <claytonsiby@gmail.com>
2021-05-06 21:02:41 +0200

    Update the JsonWebToken generating file

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,18 +1,19 @@
 class JsonWebToken
   # secret to encode and decode token
   HMAC_SECRET = Rails.application.secrets.secret_key_base
+  ALGORITHM_TYPE = 'HS256'.freeze
 
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from the creation time.
     payload[:exp] = exp.to_i
 
     # sign token with application secret
-    JWT.encode(payload, HMAC_SECRET)
+    JWT.encode(payload, HMAC_SECRET, ALGORITHM_TYPE)
   end
 
   def self.decode(token)
     # get payload, first index in decoded Array
-    body = JWT.decode(token, HMAC_SECRET)[0]
+    body = JWT.decode(token, HMAC_SECRET, true, { algorithm: ALGORITHM_TYPE })
     HashWithIndifferentAccess.new body
     # rescue from all decode errors
   rescue JWT::DecodeError => e
