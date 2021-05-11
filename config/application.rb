38270b9330e7662b2c7f6bed299794b951cd111e
ClaytonSiby <claytonsiby@gmail.com>
2021-05-11 20:17:55 +0200

    Update the CORS configuration in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -37,14 +37,14 @@ module GentlemanApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource(
           '*',
           headers: :any,
           methods: [:get, :patch, :put, :delete, :post, :options]
-        )
+          )
       end
     end
   end
